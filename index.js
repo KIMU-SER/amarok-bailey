e885513b1fa8101f435a9a55066c28e805e8021a
KIMU-SER <113019867+KIMU-SER@users.noreply.github.com>
2022-10-05 14:31:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,100 +1,48 @@
-const { WAConnection, MessageType } = require('@adiwajshing/baileys').default
-const makeWASocket = require("@adiwajshing/baileys").default
-const { exec, spawn, execSync } = require("child_process")
-const pino = require('pino')
-let qrcodee = require('qrcode')
-const fs = require('fs')
-const aes256 = require('aes256');
-const qrcode = require("qrcode-terminal")
-const { delay, useSingleFileAuthState } = require("@adiwajshing/baileys")
-exec('rm -rf session.json')
-exec('rm -rf qr.png')
-const { state, saveState } = useSingleFileAuthState('./session.json')
-var PastebinAPI = require('pastebin-js'),
-    pastebin = new PastebinAPI({
-      'api_dev_key' : 'u_53edsqmFGKd02RMyQPwONVG0bIPi-R',});
-var express = require('express');
-const path = require('path')
-var router = express.Router();
-const port = process.env.PORT || 3000;
-/* GET home page. */
-router.get('/', function(req, res, next) {
-var options = {
 
-        root: path.join()
+let cluster = require('cluster')
+let path = require('path')
+let fs = require('fs')
+const Readline = require('readline')
 
-};
-  function inrlmd() {
-    let inrl = makeWASocket({
-      logger: pino({ level: 'silent' }),
-      auth: state,
-      printQRInTerminal: false,
-      browser: ["ᴀᴍᴀʀᴏᴋ-ᴍᴅ", "Microsoft", "3.0.0"]
+var isRunning = false
+/**
+ * Start a js file
+ * @param {String} file `path/to/file`
+ */
+function start(file) {
+  if (isRunning) return
+  isRunning = true
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+
+  cluster.setupMaster({
+    exec: path.join(__dirname, file),
+    args: args.slice(1),
+  })
+  let p = cluster.fork()
+  p.on('message', data => {
+    console.log('[RECEIVED]', data)
+    switch (data) {
+      case 'reset':
+        p.kill()
+        isRunning = false
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+  p.on('exit', code => {
+    isRunning = false
+    console.error('Exited with code:', code)
+    if (code === 0) return
+    fs.watchFile(args[0], () => {
+      fs.unwatchFile(args[0])
+      start(file)
     })
-    inrl.ev.on("connection.update", async (s) => {
-    
-    if(s.qr) {
-                  qrcodee.toFile('./qr.png', s.qr, {
-                      color: {
-                          dark: '#000',
-                      }
-                  })
-              }
-              
-      const { connection, lastDisconnect } = s
-      if (connection == "open") {
-        await delay(1000 * 10)
-        const session = fs.readFileSync('./session.json') 
-      
-          pastebin .createPasteFromFile( './session.json')
-            .then((data) => {
-          // we have successfully pasted it. Data contains the id
-              st = (data);
-  
-  let mdmm =st. replaceAll("https://pastebin.com/", "");
-              
-          var st = (mdmm);
-  var encodedString = btoa(st);
-  var key = 'my passphrase';
-  var plaintext =(encodedString);
-  var encryptedPlainText = aes256.encrypt(key, plaintext);
-  let mdmmm = encryptedPlainText. replaceAll("==", "");
-  
-  const templateButtons = [
-      {index: 1, urlButton: {displayText:'ᴄᴏᴘʏ sᴇssɪᴏɴ', url: 'https://www.whatsapp.com/otp/copy/'+("AMAROK~"+mdmmm)}},
-      {index: 2, urlButton: {displayText: 'ɢɪᴛ ʟɪɴᴋ', url: 'https://github.com/Diegoson/AMAROK-MD'}},
-      {index: 3, urlButton: {displayText: 'ᴄᴏɴᴛᴀᴄᴛ', url: 'https://wa.me/2768688109'}},
-  ]    
-   const templateMessage = {
-      text: "☯︎ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴏᴏsɪɴɢ☯︎\n             ✰ ᴀᴍᴀʀᴏᴋ-ᴍᴅ✰\n\n        ☯︎ғᴏʀᴋ ᴀᴍᴀʀᴏᴋ ᴍᴅ ʀᴇᴘᴏ☯︎\n\n ⍟ᴄᴏᴘᴘʏ ᴛʜᴇ sᴇssɪᴏɴ_ɪᴅ⍟\n\n            ☯︎ᴛʜᴀɴᴋs☯︎",
-    
-      footer: 'ᴀᴍᴀʀᴏᴋ-ᴍᴅ,
-      templateButtons: templateButtons
-  }
-  const reply = (take) => { 
-     inrl.sendMessage(inrl.user.id, templateMessage)
-  }
-  reply(templateMessage);
-     //console.log("AMAROK~"+mdmmm);
-      })
-      }
-      if (
-        connection === "close" &&
-        lastDisconnect &&
-        lastDisconnect.error &&
-        lastDisconnect.error.output.statusCode != 401
-      ) {
-        inrlmd()
-      }
-    })
-    inrl.ev.on('creds.update', saveState)
-    inrl.ev.on('messages.upsert', () => { })
-  }
-  inrlmd()
+  })
 
-  res.sendFile('./qr.png', options)
-});
+}
 
 
-
-module.exports = router;
+start('main.js')
